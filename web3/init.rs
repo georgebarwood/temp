c1d6652d977863e9071b1f04e11aafa828c7728c
georgebarwood <46846948+georgebarwood@users.noreply.github.com>
2026-07-26 17:22:45 +0100

    Add files via upload

--- web3/init.rs
+++ web3/init.rs
@@ -1,7 +1,41 @@
 pub const INITSQL: &str = r###"
 schema web go
+schema handler go
+schema info go
+table info.handlers( Schema int, Name string ) go
+
+
 fn web.Main() -> string {
-   select 'Hello George'
+   let path = sys.arg(0,'')
+   let path = sys.substr( path, 1, 99 )
+   let sql = 'let x = handler.' | path | '()'
+
+   let x = sys.execute(sql)
 }
+
+fn web.Header() -> string {
+   select '<p>Links <a href="/admin">Menu</a> <a href="/execute">Exec</a>'
+}
+
+go 
+
+fn handler.admin() -> string {
+   let x = web.Header()
+}
+
+fn handler.execute() -> string {
+   let x = web.Header()
+   let sql = sys.arg(2, 'sql')
+
+   select '<p><b>Execute Sql</b>
+  <p><form method=post>
+     <textarea name=sql>', sql, '</textarea>
+  <p><input type=submit value=Go>
+  </form><p>
+  '
+
+  let x = sys.execute(sql)
+}
+
 
 "###;
